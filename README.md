415cbb6a44a1e1e034483135d4bb4b19338ec1db
Atsuki USHIDA <>
2020-01-16 22:15:01 +0900

    add index.js

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # express-redirect-flash
+This middlware meke it possible to redirect requests with flash attributes.
+
+## Syntax
+
+```js
+res.redirectFlash([status,] url, data)
+```
+
+## Usage
+### app.js
+```js
+const session = require('express-session')
+const redirectFlash = require('express-redirect-flash')
+
+app.use(session({
+  // some options
+}))
+app.use(redirectFlash())
+```
+### router.js
+```js
+router.post('resources/update' (req, res) => {
+  // update resources in the database
+
+  // redirect with the flash attributes
+  res.redirectFlash(302, 'resources/show', {
+    key1: 'value1',
+    key2: 'value2'
+  })
+})
+
+router.get('resources/show',  (req, res) => {
+  // Get the flash attribute `key1`
+  const v1 = res.locals.key1
+
+  // render with the flash attributes
+  res.render('view/resources')
+})
+```
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+module.exports = (app) => {
+  // Implement of the method `redirectFlash([status], url, data)`
+  const redirectFlashImpl = (req, res) => {
+    return (...args) => {
+      // Defaults of arguments
+      let status = 302
+      let url = '#'
+      let data = {}
+      // Attach the arguments
+      for(argv of args) {
+        if('number' === typeof argv) {
+          status = argv | 0
+        } else if('string' === typeof argv) {
+          url = argv
+        } else if(argv && 'object' == typeof argv) {
+          Object.assign(data, argv);
+        }
+      }
+      // Set data into the session
+      req.session.redirectFlash = data
+      // Redirect
+      res.redirect(status, url)
+    }
+  }
+  // redirect-flash-middleware
+  return (req, res, next) => {
+    // Get data from the session
+    Object.assign(res.locals, req.session.redirectFlash)
+    // Clear data in the session
+    req.session.redirectFlash = {}
+    // Add the method `redirectFlash([status], url, data)`
+    res.redirectFlash = redirectFlashImpl(req, res)
+    next()
+  }
+}
